de8fafbaf577039e17af4bfff4551483718a2427
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2025-12-17 15:41:08 -0600

    Experimentando con matrices en task7

--- src/task7.js
+++ src/task7.js
@@ -6,6 +6,24 @@ const arr = [
   
   // Type your code below this line!
   
+  //Agregando un elemento a la fila 2 con push
+  console.log("Agregando elemento a fila: ");
+  arr[2].push(30);
+  console.log(arr);
+
+  //Agregando una fila completa de numeros al array
+  console.log("Agregando fila completa de numeros: ");
+  arr.push([31,32,33,34,35]);
+  console.log(arr);
   
+  //Eliminando un elemento de una fila en especifico
+  console.log("Eliminando un elemento de una fila en especifico: ");
+  arr[3].splice(1,1);
+  console.log(arr);
+
+  //Invertir una de las filas sin afectar el resto
+  console.log("Invertir una de las filas: ")
+  arr[3].reverse();
+  console.log(arr);
   
   // Type your code above this line!
