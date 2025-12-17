df3b2f6abc8e2f2a650ca86ff74fe5ee6e21640b
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2025-12-17 14:49:14 -0600

    Llenando arreglo en task1 con numeros del 1 al 20

--- src/task1.js
+++ src/task1.js
@@ -2,7 +2,11 @@ const arr = []
 
 // Escribe tu codigo por debajo de esta linea
 
+for (let i = 1; i <= 20; i++) {
+  arr.push(i);
+}
 
+console.log(arr);
 
 // Escribe tu codigo por encima de esta linea
 
@@ -10,3 +14,4 @@ const arr = []
 export function taskArr(){
   return arr;
 }
+
